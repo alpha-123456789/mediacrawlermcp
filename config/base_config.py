d914379d8c7f14739c412b09cb7fbd8a72077fb0
chenxiang <1447301818@qq.com>
2026-03-27 09:41:35 +0800

    SAVE_DATA_OPTION

--- config/base_config.py
+++ config/base_config.py
@@ -71,7 +71,7 @@ BROWSER_LAUNCH_TIMEOUT = 60
 AUTO_CLOSE_BROWSER = True
 
 # Data saving type option configuration, supports: csv, db, json, jsonl, sqlite, excel, postgres. It is best to save to DB, with deduplication function.
-SAVE_DATA_OPTION = ""  # csv or db or json or jsonl or sqlite or excel or postgres
+SAVE_DATA_OPTION = "jsonl"  # csv or db or json or jsonl or sqlite or excel or postgres
 
 # Data saving path, if not specified by default, it will be saved to the data folder.
 SAVE_DATA_PATH = ""
